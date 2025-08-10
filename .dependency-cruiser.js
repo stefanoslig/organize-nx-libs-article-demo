f65aa454ca34301277eb49cb659e503cd8443cc8
stefanoslig <stefanoslignos@gmail.com>
2025-08-10 15:45:14 +0200

    dependency cruiser

--- .dependency-cruiser.js
+++ .dependency-cruiser.js
@@ -144,6 +144,7 @@ module.exports = {
           '(^|/)[.][^/]+[.](?:js|cjs|mjs|ts|cts|mts|json)$',                  // dot files
           '[.]d[.]ts$',                                                       // TypeScript declaration files
           '(^|/)tsconfig[.]json$',                                            // TypeScript config
+          '(^|/)test-setup.ts|polyfills.ts|environment.prod.ts$',            // Angular related
           '(^|/)(?:babel|webpack)[.]config[.](?:js|cjs|mjs|ts|cts|mts|json)$' // other configs
         ]
       },
